78850cfe26da76b2330278f3c9fcafbbad31c621
Unknown <peeranont.ja@tnis.com>
2018-06-26 16:29:10 +0700

    Lesson #1
    - Add Fragment
    - Use Basic Layouts
         - Relative Layout
         - ImageVIew
         - Button
         - TextView

--- app/src/main/java/com/example/tnis/tngpworldcup/MainFragment.kt
+++ app/src/main/java/com/example/tnis/tngpworldcup/MainFragment.kt
@@ -1,4 +1,35 @@
 package com.example.tnis.tngpworldcup
 
-class MainFragment {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): MainFragment {
+            val fragment = MainFragment()
+            val args = Bundle()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_main,
+                container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpView()
+    }
+
+    private fun setUpView() {
+
+    }
 }

c5939cc42036671b8e7f84bcaa185f94b26656d2
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-06-26 18:32:09 +0700

    Add Toolbar
    - Add toolbar in DetailActivity & XML
    - Create new theme style
    - Change AppTheme to NoActionBar

--- app/src/main/java/com/example/tnis/tngpworldcup/DetailActivity.kt
+++ app/src/main/java/com/example/tnis/tngpworldcup/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.tnis.tngpworldcup
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity: AppCompatActivity() {
 
@@ -9,10 +10,14 @@ class DetailActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        toolbar.title = "ข้อมูลการแข่งขัน"
+
+
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.container, DetailFragment.newInstance())
                 .commit()
     }
+
 
 }

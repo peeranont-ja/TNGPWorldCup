5c20c311859c9bb1f22a126640e17f260f99d2f4
vorabhol chuaybhan <vorabhol.ch@tnis.com>
2018-06-27 16:26:24 +0700

    Add Interceptor
    -Implement Interceptor without formatting

---
+++ app/src/main/java/com/example/tnis/tngpworldcup/Datamanager/Intercep/LogJsonInterceptor.kt
@@ -0,0 +1,22 @@
+package com.example.tnis.tngpworldcup.Datamanager.Intercep
+
+import android.util.Log
+import com.example.tnis.tngpworldcup.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.ResponseBody
+
+class LogJsonInterceptor: Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain?): Response  {
+        val request: Request? = chain?.request()
+        val response: Response? = chain?.proceed(request)
+        val rawJson: String = response?.body()!!.string()
+        Log.d("Intercep: ", String.format("raw JSON response is: %s", rawJson))
+        return response.newBuilder()
+                .body(ResponseBody.create(response.body()!!.contentType(), rawJson))
+                .build()
+    }
+
+}
--- app/src/main/java/com/example/tnis/tngpworldcup/Datamanager/Remote/ServiceFactory.kt
+++ app/src/main/java/com/example/tnis/tngpworldcup/Datamanager/Remote/ServiceFactory.kt
@@ -1,15 +1,20 @@
 package com.example.tnis.tngpworldcup.Datamanager.Remote
 
+import com.example.tnis.tngpworldcup.Datamanager.Intercep.LogJsonInterceptor
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ServiceFactory {
     companion object {
         fun worldCupService() : WorldCupService  {
+            val httpClient = OkHttpClient.Builder()
+            httpClient.addInterceptor(LogJsonInterceptor())
             val retrofit: Retrofit = Retrofit.Builder()
                     .baseUrl("https://mblwc.cleverapps.io/")
                     .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                    .client(httpClient.build())
                     .build()
             return retrofit.create(WorldCupService::class.java)
         }

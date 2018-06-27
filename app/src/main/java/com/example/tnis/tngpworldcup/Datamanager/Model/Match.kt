6291c3349232404f7d9c4d14a0f4f5508ee1f068
vorabhol chuaybhan <vorabhol.ch@tnis.com>
2018-06-27 11:02:49 +0700

    Add Dependencies and  Create model Class
    -Add Gson dependencies
    -Add Data Model Match class with using serialize

---
+++ app/src/main/java/com/example/tnis/tngpworldcup/Datamanager/Model/Match.kt
@@ -0,0 +1,15 @@
+package com.example.tnis.tngpworldcup.Datamanager.Model
+
+import com.google.gson.annotations.SerializedName
+
+data class Match(
+        @SerializedName("match_no") val matchNo: Int,
+        @SerializedName("match_type") val matchType: String,
+        @SerializedName("start_timestamp") val startTimeeStamp: String,
+        @SerializedName("home_team") val homeTeam: String,
+        @SerializedName("away_team") val awayTeam: String,
+        @SerializedName("home_team_name") val homeTeamName: String,
+        @SerializedName("away_team_name") val awayTeamName: String,
+        @SerializedName("home_score") val homeScore: Int,
+        @SerializedName("away_score") val awayScore: Int
+)

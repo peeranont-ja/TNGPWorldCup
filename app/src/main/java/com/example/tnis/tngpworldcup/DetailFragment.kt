15d6d4a1b416839ce794632d1d3cd60e94706581
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-06-27 18:01:08 +0700

    Design Detail Screen
    - Add Circle ImageView Library

--- app/src/main/java/com/example/tnis/tngpworldcup/DetailFragment.kt
+++ app/src/main/java/com/example/tnis/tngpworldcup/DetailFragment.kt
@@ -5,10 +5,17 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_main.*
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.centerCrop
+import com.bumptech.glide.request.RequestOptions
 
-class DetailFragment: Fragment(){
+
+
+class DetailFragment : Fragment() {
     companion object {
-        fun newInstance(): DetailFragment{
+        fun newInstance(): DetailFragment {
             return DetailFragment()
         }
     }
@@ -19,5 +26,24 @@ class DetailFragment: Fragment(){
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_detail,
                 container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpView()
+    }
+
+    private fun setUpView(){
+        val options = RequestOptions().fitCenter()
+
+        Glide.with(context!!)
+                .load("https://mblwc.cleverapps.io/images/RUS.png")
+                .apply(options)
+                .into(img_home_team)
+
+        Glide.with(context!!)
+                .load("https://mblwc.cleverapps.io/images/KSA.png")
+                .apply(options)
+                .into(img_away_team)
     }
 }

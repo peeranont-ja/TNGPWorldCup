847f739c5062d1ed820710606dcdd67a898e569e
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-06-28 17:15:44 +0700

    Create Match Response

--- app/src/main/java/com/example/tnis/tngpworldcup/datamanager/remote/model/MatchResponse.kt
+++ app/src/main/java/com/example/tnis/tngpworldcup/datamanager/remote/model/MatchResponse.kt
@@ -1,4 +1,15 @@
-package com.example.tnis.tngpworldcup.DataManager.Remote.Model
+package com.example.tnis.tngpworldcup.datamanager.remote.model
 
-class MatchResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class MatchResponse (
+        @SerializedName("match_no") val matchNo: Int,
+        @SerializedName("match_type") val matchType: String,
+        @SerializedName("start_timestamp") val startTimeStamp: String,
+        @SerializedName("home_team") val homeTeamShortName: String,
+        @SerializedName("away_team") val awayTeamShortName: String,
+        @SerializedName("home_team_name") val homeTeamName: String,
+        @SerializedName("away_team_name") val awayTeamName: String,
+        @SerializedName("home_score") val homeScore: Int,
+        @SerializedName("away_score") val awayScore: Int
+)
